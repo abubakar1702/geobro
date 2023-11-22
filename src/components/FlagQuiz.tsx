035c808a224344caa30c6cf3b1a2948af400d2c7
abubakar1702 <abubakar.akash17@gmail.com>
2023-11-23 00:16:33 +0600

    update: button sizing

--- src/components/FlagQuiz.tsx
+++ src/components/FlagQuiz.tsx
@@ -201,6 +201,7 @@ export default function FlagQuiz() {
                         width: "100%",
                         height: "100%",
                         objectFit: "contain",
+                        padding: "10px"
                       }}
                     />
                   </div>
@@ -212,7 +213,7 @@ export default function FlagQuiz() {
                 <div className="flex flex-col">
                   {fourRandomCountry.slice(0, 2).map((country, index) => (
                     <button
-                      className="p-4 font-bold h-24 w-40 border-solid border-4 border-sky-600 rounded-md m-2"
+                      className="p-4 font-bold h-28 w-40 border-solid border-4 border-sky-600 rounded-md m-2"
                       onClick={() => handleAnswer(country)}
                       key={index}
                     >
@@ -223,7 +224,7 @@ export default function FlagQuiz() {
                 <div className="flex flex-col">
                   {fourRandomCountry.slice(2, 4).map((country, index) => (
                     <button
-                      className="p-4 font-bold w-40 h-24 border-solid border-4 border-sky-600 rounded-md m-2"
+                      className="p-4 font-bold w-40 h-28 border-solid border-4 border-sky-600 rounded-md m-2"
                       onClick={() => handleAnswer(country)}
                       key={index + 2}
                     >
